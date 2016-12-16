a05787f36ffb3dc578bed0dfa21121ea62138a4e
980f <andy.980f@gmail.com>
2016-12-15 21:13:04 -0600

    skeleton for removing dependency on Glib::ustring

---
+++ ustring.cpp
@@ -0,0 +1,17 @@
+#include "ustring.h"
+
+Ustring::Ustring(){
+
+}
+
+Ustring::Ustring(const char *content){
+
+}
+
+Ustring::Ustring(const Ustring &other){
+
+}
+
+char *Ustring::cstr(){
+  return nullptr;
+}
---
+++ ustring.h
@@ -0,0 +1,16 @@
+#ifndef USTRING_H
+#define USTRING_H
+
+/** wrapper for common subset of classes like Glib::ustring and std::string
+the implementation file here is a simple one, but may integrate with UTF* processing classes.
+*/
+class Ustring {
+public:
+  Ustring();
+  Ustring(const char *content);
+  Ustring(const Ustring &other);
+  /** pointer to raw storage, presumed to be null terminated */
+  char * cstr();
+};
+
+#endif // USTRING_H
