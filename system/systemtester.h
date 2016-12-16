586de26bdb3c1d6c0383ca36383fc279272262fd
980f <andy.980f@gmail.com>
2016-12-15 21:11:18 -0600

    testing pre-commit hook

--- system/systemtester.h
+++ system/systemtester.h
@@ -1,11 +1,18 @@
 #ifndef SYSTEMTESTER_H
 #define SYSTEMTESTER_H
 
+#include "storable.h"
+
 class SystemTester
 {
 
 public:
   SystemTester();
+  void testStorable(){
+    Storable node("safely system library");
+    node.child("a chld").setNumber(15.678);
+    printf("Stored: ",node.image().c_str());
+  }
 };
 
 
