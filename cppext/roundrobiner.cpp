ee19354fa73fba7ba37f41e4a21e2b1fb5b47ec9
980f <andy.980f@gmail.com>
2017-01-04 19:54:55 -0600

    int to unsigned

--- cppext/roundrobiner.cpp
+++ cppext/roundrobiner.cpp
@@ -3,7 +3,7 @@
 #include "cheaptricks.h"
 #include "minimath.h" //fill's clear's
 
-RoundRobiner::RoundRobiner(bool *Scoreboard, int quantity) : Scoreboard(Scoreboard), quantity(quantity){
+RoundRobiner::RoundRobiner(bool *Scoreboard, unsigned quantity) : Scoreboard(Scoreboard), quantity(quantity){
   markAll(0);
   last = BadLength;
 }
@@ -15,8 +15,8 @@ bool *RoundRobiner::deallocate(){
 }
 
 /** carefully crafter to ensure that even if the scoreboard is set fully between calls to this function this function will once per cycle return 'nothing'*/
-int RoundRobiner::next(void){
-  for(int n = last; ++n < quantity; ) {
+unsigned RoundRobiner::next(void){
+  for(unsigned n = last; ++n < quantity; ) {
     if(flagged(Scoreboard[n])) {
       return last = n; //#assign and return
     }
@@ -24,20 +24,20 @@ int RoundRobiner::next(void){
   return last = BadLength; //magic value for 'none'
 } /* next */
 
-bool &RoundRobiner::bit(int id) const {
+bool &RoundRobiner::bit(unsigned id) const {
   static bool trashme;
-  if(id < 0 || id >= quantity) {
+  if(id >= quantity) {
     return trashme;
   }
   return Scoreboard[id];
 }
 
-bool RoundRobiner::post(int id){
-  if(id==~0) {//hack for occasional "reportAll" functionality.
+bool RoundRobiner::post(unsigned id){
+  if(id==ALL) {//hack for occasional "reportAll" functionality.
     markAll();
     return true;
   }
-  if(id < 0 || id >= quantity) {
+  if(id >= quantity) {
     return false;
   }
   Scoreboard[id] = true;
