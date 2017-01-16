6c42f51b81892e39e346e4155cbcec1511477c56
andyh <andy.980f@gmail.com>
2017-01-16 00:25:12 -0600

    more wrong lib stuff, trying to get 'system' to actually be 'embeddable'

---
+++ posix/posixlogger.cpp
@@ -0,0 +1,6 @@
+#include "posixlogger.h"
+
+PosixLogger::PosixLogger()
+{
+
+}
---
+++ posix/posixlogger.h
@@ -0,0 +1,11 @@
+#ifndef POSIXLOGGER_H
+#define POSIXLOGGER_H
+
+
+class PosixLogger
+{
+public:
+  PosixLogger();
+};
+
+#endif // POSIXLOGGER_H
