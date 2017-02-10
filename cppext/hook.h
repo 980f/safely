d0446ba7a8df90bf898a660b29a9d87f90d73aa9
andyh <andy.980f@gmail.com>
2017-02-09 21:01:48 -0600

    hook class works, at least the version with a return does.

---
+++ cppext/hook.h
@@ -0,0 +1,52 @@
+#ifndef HOOK_H
+#define HOOK_H
+
+/** hook with a return value */
+template<typename RetType, typename ... Args> class Hooker {
+  typedef RetType (*Pointer)(Args ...);
+  Pointer pointer;
+  RetType defaultReturn;
+ public:
+  Hooker(RetType nullAction,Pointer fn=nullptr):pointer(fn),defaultReturn(nullAction){}
+  /** set the function pointer.
+   * Note that the default value remains that which was set by the constructor. This makes sense as the default is what the owner of the hook chooses, not the individual hoping to use the hook.
+   * @returns the old pointer, for those usages which are 'borrowing' the hook, or nice enough to share with previous one (for which you shohuld consider using sigc library). */
+  Pointer operator =(Pointer fn){
+    Pointer was=pointer;
+    pointer=fn;
+    return was;
+  }
+
+  RetType operator () (Args ... args){
+    if(pointer){
+      return (*pointer)(args ...);
+    } else {
+      return defaultReturn;
+    }
+  }
+
+};
+
+/** until I figure out how to code a void type for a return in Hooker here is a simpler version of that */
+template<typename ... Args> class Hook {
+  typedef void (*Pointer)(Args ...);
+  Pointer pointer=nullptr;
+ public:
+  Hook(Pointer fn):pointer(fn){}
+
+  Pointer operator =(Pointer fn){
+    Pointer was=pointer;
+    pointer=fn;
+    return was;
+  }
+
+  void operator () (Args ... args){
+    if(pointer){
+      (*pointer)(args ...);
+    }
+  }
+
+};
+
+
+#endif // HOOK_H
