b59203fd6e9669623f0a0a1dd10c92c63ac06b34
980f <andy.980f@gmail.com>
2017-03-19 17:13:59 -0500

    cleanup of ensuring zero terminated text input- pushed to Indexer where it can be used elsewhere

--- posix/filereader.h
+++ posix/filereader.h
@@ -8,19 +8,15 @@
 #include "fildes.h" //our file <-> safe buffer routines
 #include "hook.h"  //used for callbacks to process file.
 /** mate aio calls to FilDes class, initially for reader, will likely rename and refactor etc.
- * This base is geared towards whole file read and writes, I will add classes for 'big file' reading/wrriting.
+ * This base is geared towards whole file read and writes, I will add classes for 'big file' reading/writing.
 */
 class FileAsyncAccess: public PosixWrapper {
   /** transfer direction. */
   const bool amReader;//const until we can make sense of changing direction while open.
   /** the file */
   Fildes &fd;
-  /** the data source */
-  Indexer<u8> &buf;//unsharing object
-
-  /** @deprecated: move logic to where buffer is allocated
-   *  convenience to protect last bytes of buffer, typically where a null terminator is placed. */
-  unsigned guard;
+  /** a view into the data source or sink */
+  Indexer<u8> &buf;
 
   typedef Hooker<bool /*go again*/,__ssize_t /*exitcode or number of bytes read */> OnCompletion;
   /** what to call when operation is completed.
@@ -39,7 +35,7 @@ public:
   /* connect to user allocated file and buffer objects. This makes it easier to change synch code to async. */
   FileAsyncAccess(bool reader,Fildes &fd,Indexer<u8> &buf,OnCompletion::Pointer onDone);
   /** start read process */
-  bool go(unsigned guard=1);//reserve place for a null
+  bool go();//reserve place for a null
 
   /** having trouble sometimes putting this in constructor arg list: */
   void setHandler(OnCompletion::Pointer onDone);
