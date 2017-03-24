740cd78f425807a4674c916498a902afa233d267
980f <andy.980f@gmail.com>
2017-03-23 19:28:05 -0500

    abandoning static libs, tired of chasing link order issues, at the present posixlogger pieces were getting not found even though clearly present

--- wiringPi/main.cpp
+++ wiringPi/main.cpp
@@ -1,12 +1,19 @@
 #include "gpio.h"
+#include "logger.h"
 
-int main(int argc, char *argv[])
-{
+static Logger out("MAIN");
+int main(int argc, char *argv[]){
+
+  for(int argi=0;argi<argc;++argi){
+    out("arg[%d]=%s",argi,argv[argi]);
+  }
   if(MemoryMapper::init(true)){
     GPIO somebit(21);//todo: safe test bit  0..27 seem to be ok 21 is handy
+    out("Created bit ");
     somebit.configure(1,0);//simple output
-
+    out("Configure as output");
     somebit.toggle();
+    out("Toggled it");
   }
   return 0;
 }
