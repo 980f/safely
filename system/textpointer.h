bedab0c432f943ee0645417f62ee4b6bd777ca55
andyh <andy.980f@gmail.com>
2017-04-29 20:33:53 -0500

    better comments and protecting a dangerous member

--- system/textpointer.h
+++ system/textpointer.h
@@ -9,7 +9,7 @@
  *
  * adds strdup'ing to @see Cstr functionality, i.e. makes a copy on construction and assignement vs Cstr which just looks at someone else's allocated memory.
  *
- *  This class unconditionally frees the data it points at when destroyed. IE it always owns what it points at.
+ *  This class unconditionally frees the data it points at when destroyed. IE it always owns what it points at, which is why it usually copies its argument on assignment and construction.
  */
 class Text : public Cstr {
 
@@ -66,17 +66,16 @@ public:
 
   /** make a copy of @param other. If other points to the same memory as this ... we might screw up */
   void copy(TextKey other);
-
-  /** relinquish ownership */
+private:
+  /** relinquish ownership, which entails also forgetting the content. If used wrongly this will leak memory. */
   void release(){
     ptr=nullptr;
   }
-
+public:
   /** discard==free internal content (if any) and null the internal pointer (to prevent use-after-free) */
   void clear() noexcept override;
 
-
-  /** a core part of a parser is to take substrings, this does just that part of parsing */
+  /** a core part of a parser is to take substrings, this does just that part of parsing. It never retains the content it manipulates, it creates copies that it hands to you. */
   class Chunker:public Span {
     Cstr base;
   public:
