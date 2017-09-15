02100b3b7311a754d9cb6543b12d47e257898ac7
andyh <andy.980f@gmail.com>
2017-09-15 14:20:13 -0500

    recovered from copying cpp over h

--- cppext/textchunk.cpp
+++ cppext/textchunk.cpp
@@ -1,10 +1,34 @@
 #include "textchunk.h"
 #include "string.h"
 
+TextBlock::TextBlock(char *start, unsigned allocated, bool amOwner):
+  start(*start),
+  allocated(allocated),
+  amOwner(amOwner)
+{
+
+}
+
 TextBlock::~TextBlock(){
   if(amOwner){
     delete [](&start);
   }
+}
+
+TextBlock::TextBlock(TextBlock &&other):
+  start(other.start),
+  allocated(other.allocated),
+  amOwner(false)
+{
+
+}
+
+TextBlock::TextBlock(const TextBlock &other):
+  start(other.start),
+  allocated(other.allocated),
+  amOwner(false)
+{
+
 }
 
 int TextBlock::cmp(const char *other) const noexcept {
@@ -17,7 +41,7 @@ bool TextBlock::operator==(const char *other) const noexcept{
 
 unsigned TextBlock::nextChar(unsigned from, char toseek) const noexcept{
   for(;from<allocated;++from){
-    if(at(from)==toseek){
+    if(*at(from)==toseek){
       return from;
     }
   }
@@ -27,9 +51,15 @@ unsigned TextBlock::nextChar(unsigned from, char toseek) const noexcept{
 TextChunk::operator TextBlock() const noexcept{
   Span safe=Span::overlap(span,Span(0,block.allocated));
   if(safe.ordered()){//normal case, a substring
-    return TextBlock(&block.at(safe.lowest),span.span());
+    return TextBlock(block.at(safe.lowest),span.span());
   } else {
     return TextBlock(nullptr,0);
   }
 
+}
+
+void TextChunk::next(char comma){
+//"1" is sizeof(char):
+  span.leapfrog(1);//points after previous find, or to start of block
+  span.highest=block.nextChar(span.lowest,comma);
 }
--- cppext/textchunk.h
+++ cppext/textchunk.h
@@ -1,49 +1,40 @@
-#ifndef TEXTCHUNK_H
-#define TEXTCHUNK_H
-
 #include "halfopen.h"
 
-/** minimalist block of text. is intrinisically const once created, although the referenced content is mutable */
-struct TextBlock {
+#pragma once
+
+class TextBlock {
   friend class TextChunk;
+public:
   char &start;
   const unsigned allocated;
-  /** free on delete */
   const bool amOwner;
-  /** @param yours is whether to free start on destruction */
-  TextBlock(char *start, unsigned allocated,bool yours=false);
-  /** copying is a good idea */
-  TextBlock(const TextBlock &other)=default;
-  /** create on the fly in an argument list */
-  TextBlock(TextBlock &&other)=default;
-  /** potentially delete the buffer, if was given ownership */
+
+  TextBlock(char *start,unsigned allocated,bool amOwner = false);
   ~TextBlock();
-  /** strncompare */
+  TextBlock(TextBlock &&other);
+  TextBlock(const TextBlock &other);
+
   int cmp(const char *other) const noexcept;
+
   bool operator==(const char *other) const noexcept;
-  /** @returns index of next instance of @param toseek that is at or follows @param from */
-  unsigned nextChar(unsigned from, char toseek) const noexcept;
-  /** safe indexing function */
-  char operator [](unsigned index) const noexcept {
+
+  char operator [](unsigned index)const noexcept{
     return index<allocated?(&start)[index]:0;
   }
-protected:
-  /** unsafe index function, only call when you have constrained @param index */
-  char &at(unsigned index) const noexcept {
-    return (&start)[index];
-  }
-};
 
-/** a block of text and a sliding window over it */
+  unsigned nextChar(unsigned from, char toseek) const noexcept;
+protected:
+  char *at(unsigned index)const noexcept{
+    return &((&start)[index]);
+  }
+}; // class TextBlock
+
 class TextChunk {
 public:
   TextBlock &block;
   Span span;
-public:
-  TextChunk(const TextBlock &block);
-  /** @returns a block that is safely within its own block, the whole block is span is not valid.
- if you want an empty block on an invalid span then check before calling this */
-  operator TextBlock() const noexcept ;
-};
+  TextChunk(TextBlock &block);
+  operator TextBlock() const noexcept;
 
-#endif // TEXTCHUNK_H
+  void next(char comma);
+};
