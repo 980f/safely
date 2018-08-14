702a92c89799a810b1cb09272cc6fb7cb7f61d46
980f <andy.980f@gmail.com>
2018-08-14 18:57:47 -0500

    typo in comment

--- cppext/minimath.h
+++ cppext/minimath.h
@@ -201,11 +201,11 @@ unsigned digitsAbove(unsigned int value, unsigned numDigits);
 /** an integer power of 10. out of bounds arg gets you nothing but trouble ... */
 u64 i64pow10(unsigned power);
 
-/** @param p19 is 1-^19 times a fractional value. @param digits is the number of digits past the virtual radix point you are interested in.
+/** @param p19 is 10^19 times a fractional value. @param digits is the number of digits past the virtual radix point you are interested in.
  *  @returns a properly rounded int that has those digits of interest, but you may need to pad with leading zeroes. */
 u64 keepDecimals(u64 p19,unsigned digits);
 
-/** @param p19 is 1-^19 times a fractional value. @param digits is the number of digits past the virtual radix point you are interested in.
+/** @param p19 is 10^19 times a fractional value. @param digits is the number of digits past the virtual radix point you are interested in.
  *  @returns a truncated int that has those digits of interest, but you may need to pad with leading zeroes. */
 u64 truncateDecimals(u64 p19,unsigned digits);
 /** filtering in case we choose to optimize this */
