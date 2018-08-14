3cb36dd8147fc78213f5241f0a9bf8cef8e55ed5
980f <andy.980f@gmail.com>
2018-08-14 18:40:11 -0500

    regression tester for number parsing

---
+++ cppext/testnumberparsing.cpp
@@ -0,0 +1,6 @@
+#include "testnumberparsing.h"
+
+TestNumberParsing::TestNumberParsing()
+{
+
+}
---
+++ cppext/testnumberparsing.h
@@ -0,0 +1,11 @@
+#ifndef TESTNUMBERPARSING_H
+#define TESTNUMBERPARSING_H
+
+
+class TestNumberParsing
+{
+public:
+  TestNumberParsing();
+};
+
+#endif // TESTNUMBERPARSING_H
