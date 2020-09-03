fb56c9d00028d34b57e23e3211e728da19abc97d
980f <andy.980f@gmail.com>
2020-09-03 11:27:54 -0500

    dropping non-microcontroller subproject

--- rasbpi/peripheral.cpp
+++
@@ -1,7 +0,0 @@
-#include "peripheral.h"
-
-static unsigned PiSpace=0x7e000000;
-Peripheral::Peripheral(unsigned blockOffset, unsigned blockLength):
-  reg(PiSpace|blockOffset,blockLength){
-  //we could cache some items here such as validity of the mapping.
-}
