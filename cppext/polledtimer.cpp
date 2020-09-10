49f58c5e3e58a679d2ff40a54fe3f5c7c57f267e
980f <andy.980f@gmail.com>
2020-09-10 16:51:35 -0500

    imported from ezcpp, requires linker magic

--- cppext/polledtimer.cpp
+++ cppext/polledtimer.cpp
@@ -1,23 +1,45 @@
 #include "polledtimer.h"
+#include "tableofpointers.h"
 #include "onexit.h"
-//#pragma GCC diagnostic warning "-Wunused-variable"
+
+#include "systick.h" //our server
+using namespace SystemTimer;
+
+#pragma GCC diagnostic warning "-Wunused-variable"
+
+/**
+  * an isr will determine that the given time has expired,
+  * but the interested code will have to look at object to determine that the event occurred.
+  * as of this note all timers are touched every cycle even if they are finished.
+  * using 2 lists would make the isr faster, but all the restarts slower and restarts dominate first use.
+  */
+
+MakeTable(PolledTimer);
+
+/** someone has to register this with Systick */
+void PolledTimerServer() {
+  ForTable(PolledTimer){
+    (*it)->check();
+  }
+} /* onTick */
+
 
 void PolledTimer::check(){
   if(running){
     if(--systicksRemaining==0){
-      onDone();//default method clears running, i.e. simple PolledTimers are one-shots.
+      onDone();
     }
   }
 }
 
-PolledTimer::PolledTimer(void){
+PolledTimer::PolledTimer(){
   running = 0;
   systicksRemaining = 0;
 }
 
-PolledTimer::~PolledTimer(){
-  //#trivial
-  running=0;//reduce use-after-free consequences
+Ticks PolledTimer::ticksForSeconds(double sec){
+  //todo: need abstract connection for this
+  return SystemTimer::ticksForSeconds(sec);
 }
 
 /** typically this is overloaded if latency is important.*/
@@ -25,7 +47,7 @@ void PolledTimer::onDone(void){
   running = 0;
 }
 
-void PolledTimer::restart(u32 value){
+void PolledTimer::restart(Ticks value){
   systicksRemaining = value + 1; //to ensure minimum wait even if tick fires while we are in this code.
   if(value > 0) {//# leave expanded for debug
     running = 1; //this makes this retriggerable
@@ -39,12 +61,14 @@ void PolledTimer::restart(float seconds){
   if(seconds<=0){
     return;
   }
-  restart(u32(seconds));
+  restart(ticksForSeconds(seconds));
 }
 
 void PolledTimer::freeze(){
   running = 0;//precludes isr touching remaining time, and onDone doesn't get called.
 }
+
+PolledTimer::~PolledTimer(){}
 
 ////////////////////////////
 
--- cppext/polledtimer.h
+++ cppext/polledtimer.h
@@ -5,7 +5,8 @@
 
 /** marker class for timing services */
 typedef u32 Ticks;
-/** shared timer services. 
+
+/** shared timer services.
   * an isr will determine that the given time has expired, setting the done bit.
   * but the interested code will have to look at each object to determine that the event occurred OR
   * will have to derive a class and overload onDone().
@@ -17,19 +18,23 @@ protected:
   bool running; //changed from 'done' to 'running' so that we can init via joint ram clear rather than init code.
   Ticks systicksRemaining;
 public:
-  PolledTimer(void);
-  virtual ~PolledTimer();//in case derived classes need significant destruction.
+  PolledTimer();
+  ~PolledTimer();//=default;
   virtual void restart(Ticks ticks);
+
   void restart(float seconds);//float (not double) as is often in time critical code.
   /** stops countdown without triggering onDone() */
   void freeze();
+
   //what the timer isr calls:
-  void check(void);
+  void check();
+
   /** called when systicksRemaining goes to 0.
    * Overload to have something done <B>within</B> the timer interrupt service routine */
-  virtual void onDone(void);
+  virtual void onDone();
+
   /** @returns whether this is no longer counting */
-  inline bool isDone() const{
+  inline bool isDone() const {
     return !running;
   }
 };
@@ -43,29 +48,36 @@ protected:
   Ticks period;
   u32 fired;
 public:
-  CyclicTimer(Ticks period,bool andStart=false):period(period){
-    if(andStart){
+  explicit CyclicTimer(Ticks period, bool andStart = false) : period(period), fired(0) {
+    if (andStart) {
       retrigger();
     }
   }
 
-  bool hasFired(void);
+  bool hasFired();
 
-  operator bool (void){
+  operator bool() { // NOLINT
     return hasFired();
   }
 
-  void retrigger(void){
+  void retrigger() {
     //leave fired as is.
     PolledTimer::restart(period);
   }
 
-  void restart(Ticks ticks){
+  void restart(Ticks ticks) override {
     PolledTimer::restart(period = ticks);
   }
 
   /** called when systicksRemaining goes to 0.
    * Overload to have something done within the timer interrupt service routine */
-  virtual void onDone(void);
-
+  void onDone() override;
 };
+
+
+/** a server that will update all registered timers.
+ * You must arrange for it to get called with each tick */
+extern void PolledTimerServer();
+//registration is compile time, your object must be static to be party to this service.
+#define RegisterTimer(name) Register(PolledTimer,name)
+
