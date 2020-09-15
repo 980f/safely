26ca5d8d9793f358240d120bb19eca4bd3a58239
980f <andy.980f@gmail.com>
2020-09-14 22:10:12 -0500

    remove a guard against an old bug, that is now a compiler warning

--- cppext/settable.cpp
+++ cppext/settable.cpp
@@ -5,23 +5,10 @@
 #define MAXARGS 15
 #endif
 
-//bool Settable::blockCheck(Settable &desired) const{
-//  return differs(desired);
-//}
-//
-//bool Settable::differs(const Settable &other)const{
-//  ArgBlock<MAXARGS> args;
-//  ArgBlock<MAXARGS> args2;
-//
-//  other.getParams(args);
-//  this->getParams(args2);
-//  return !args2.equals(args);
-//}
-
 void Settable::copy(const Settable &other){
-  if(!&other){//#warn -Waddress etc. is ok, we guard against a compiler generated operator = with a null 'other'
-    return;
-  }
+  //if(!&other){//#warn -Waddress etc. is ok, we guard against a compiler generated operator = with a null 'other'
+  //  return;
+  //}
   ArgBlock<MAXARGS> args;
   other.getParams(args);
   args.freeze();
