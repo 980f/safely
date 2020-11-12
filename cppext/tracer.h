b0a2112c12d86984c086d556f9908a87696ce34c
Jacob <jacob.osborne@rigaku.com>
2020-11-12 16:34:57 -0600

    allow const object, should use 'traits' to qualify with copyability vs copysconstructable

--- cppext/tracer.h
+++ cppext/tracer.h
@@ -10,21 +10,20 @@
  * @param TraceItem class must be copy assignable.
  * if @param Ticked is true then it must have a 'tick' member of some integral type and this class will timestamp the object.
  * */
-template <class TraceItem,unsigned depth=100,bool Ticked= true>
-class Tracer {
+template<class TraceItem, unsigned depth = 100, bool Ticked = false> class Tracer {
   TraceItem memory[depth];
 public:
-  CircularIndexer<TraceItem> looper{ memory, sizeof(memory)};
+  CircularIndexer<TraceItem> looper{memory, sizeof(memory)};
 
-  void operator ()(TraceItem &&braced){
-    if constexpr (Ticked){
+  void operator()(TraceItem &&braced) {
+    if constexpr (Ticked) {
       braced.tick = SystemTimer::snapLongTime();
     }
-    looper.next()=braced;
+    looper.next() = braced;
   }
 
-//  void operator ()(const TraceItem &braced) {
+  void operator()(const TraceItem &braced) {
 //    can't alter a const to set the tick
-//    looper.next()=braced;
-//  }
+    looper.next() = braced;
+  }
 };
