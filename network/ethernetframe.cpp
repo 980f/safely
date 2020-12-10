87300b5e58b3532a2fe8c0f92201fcbf863cda2d
andyh <andy.980f@gmail.com>
2020-12-10 17:43:45 -0600

    still undergoing initial construction

--- network/ethernetframe.cpp
+++ network/ethernetframe.cpp
@@ -1,3 +1,20 @@
 // (C) 2020 Andrew Heilveil (github/980f) Created on 10/30/20.
 
 #include "ethernetframe.h"
+
+DataBlock TcpEthernet::options() {
+  return {tcpHeader.offset * 4u, reinterpret_cast<uint8_t *>(&tcpHeader) + sizeof(TCPHeader)};
+}
+
+DataBlock TcpEthernet::payload(unsigned int totalEthernetLength) {
+  auto opts=options();
+  return {totalEthernetLength - sizeof(TcpEthernet) -opts.quantity(), opts.violated()+opts.quantity()};//todo:00 this presumes checksum has been stripped from given length
+}
+
+Block<uint8_t> IPV4Header::options() {
+  if (headerLength > 5) {
+    return {chunkLength(headerLength, 5), myWord(5)};
+  } else {
+    return {};
+  }
+}
--- network/ethernetframe.h
+++ network/ethernetframe.h
@@ -8,33 +8,56 @@
 #include <charscanner.h>
 #include "bigendianer.h"
 #include <cstring> //temp for memcpy
+#include <cgmessage.h>
 
-//on the cortexM series this will inline a single instruction.
-extern constexpr void htonl32(uint8_t *target, uint32_t source);
 
+constexpr uint16_t htons(uint16_t swapme){
+  return swapme>>8 | swapme<<8;
+}
 
 template<unsigned num> class NetworkAddress {
 protected:
   /** maintained in network order, MSbyte at lowest index */
   uint8_t octets[num];
 
-public:
-  NetworkAddress(const uint8_t *octets) {
-    memcpy(this->octets, octets, num);
+  INLINETHIS
+  NetworkAddress &copy(const uint8_t *rhs) {
+    memcpy(octets, rhs, num);
+    return *this;
   }
 
-  void makeall(bool ones) {
-    memset(octets, ones ? ~0 : 0, num);
+public:
+  NetworkAddress(const NetworkAddress<num> &rhs) {
+    copy(rhs);
+  }
+
+  NetworkAddress(const uint8_t rhs[num]) {
+    copy(rhs);
+  }
+
+  /** garbage, unless in the block lzeroed by c startup*/
+  NetworkAddress() {
+  }
+
+  NetworkAddress &operator=(const uint8_t *rhs) {
+    return copy(rhs);
+  }
+
+  NetworkAddress &operator=(const NetworkAddress<num> &rhs) {
+    return copy(rhs.octets);
+  }
+
+  NetworkAddress &operator=(NetworkAddress<num> &&rhs) {
+    return copy(rhs);
   }
 
   BigEndianer scanner() {
-    return {octets, sizeof(octets)};
+    return {octets, num};
   }
 
   void parse(Cstr humanreadable, bool hexly) {
     ByteScanner reader(humanreadable.casted(), humanreadable.length());
-    auto writer(scanner());
-    while (writer.hasNext()) {//#IDE mismarks this
+    for (auto writer(scanner()); writer.hasNext();) {//#IDE mismarks this as not looping
       auto &octet = writer.next();
       octet = 0;
       while (reader.hasNext()) {
@@ -46,6 +69,18 @@ public:
     }
   };
 
+  bool operator==(const NetworkAddress<num> &sameclass) const {
+    for (unsigned length = num; length-- > 0;) {
+      if (octets[length] != sameclass.octets[length]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool operator!=(const NetworkAddress<num> &sameclass) const {
+    return !operator==(sameclass);
+  }
 } PACKED;
 
 struct EthernetAddress : public NetworkAddress<6> {
@@ -67,28 +102,24 @@ struct IPV4Address : public NetworkAddress<4> {
 }PACKED;
 
 struct ArpV4Pair {
-  /** carefully layed out for EthernetFrame */
+  /** carefully layed out for direct use in packets */
   EthernetAddress HA;
   IPV4Address PA;
 } PACKED;
 
-enum HTYPE {
-  Ethernet = 1
-};
-
-enum EtherType {
+enum EtherType {//if these insert wrong into packets then fix that here, no need to be byteswapping constants.
   NoType = 0 //indicates uninit field.
-  , ARP_type = 0x0806
-  , IPV4_type = 0x0800 //APV4
-  , RARP_type = 0x8035 // Reverse ARP
-  , PTP = 0x88F7  //Precision Time Protocol
+  , ARP_type = htons(0x0806)
+  , IPV4_type = htons(0x0800) //IPV4
+  , RARP_type = htons(0x8035) // Reverse ARP
+  , PTP = htons(0x88F7)  //Precision Time Protocol
 };
 
 /** ARP opcodes */
 enum Arpcodes {
   NoCode = 0 //indicates uninit field.
-  , ArpRequest = 1
-  , ArpReply = 2
+  , ArpRequest = htons(1)
+  , ArpReply = htons(2)
   // 3 .. 9 for RARP and InARP and friends, add as needed.
 };
 
@@ -100,41 +131,56 @@ enum IPProtos {
   , Experimental = 253
   , Testing = 254
 };
-/**
- * gratuitous ARP: targetPA=senderPA targetHA=zip;
- * probe: senderPA&HA=self; target HA=zip and target PA is what we are looking for the address of.
- *
- * */
-struct ArpIPV4_Ether {
-  uint16_t hardwareType = 1; //wifi 6
-  uint16_t protocolType = 2048;
-  uint8_t hwAddressLength = 6;
-  uint8_t protocolAddressLength = 4;
-  uint16_t operation;
-  ArpV4Pair sender;
-  ArpV4Pair target;
-} PACKED;
 
+/** the 14 byte header */
 struct EthernetHeader {
   EthernetAddress destMac;
   EthernetAddress sourceMac;
   EtherType ethType;
 } PACKED;
 
+/** arp content
+ *
+ * FYI: gratuitous ARP: targetPA=senderPA targetHA=zip;
+ * probe: senderPA&HA=self; target HA=zip and target PA is what we are looking for the address of.
+ *
+ * */
+struct ArpIPV4 {
+  uint16_t hardwareType;// = htons(1); //wifi htons(6)
+  uint16_t protocolType;// = htons(2048);
+  uint8_t hwAddressLength;// = 6;
+  uint8_t protocolAddressLength;// = 4;
+  uint16_t operation;
+  ArpV4Pair sender;
+  ArpV4Pair target;
 
-/***/
+} PACKED;
+
+
+/** complete arp packet */
+struct ArpIPV4Ethernet {
+  EthernetHeader ethernetHeader;
+  ArpIPV4 arpIpv4;
+} PACKED;
+
+/** */
 struct IPV4Header {
   uint16_t totalLength;//576 = 512+64
+
   unsigned DSCP: 6;
   unsigned ECN: 2;
   unsigned headerLength: 4; //*4 for bytes
   unsigned version: 4;
 
-
-  unsigned fragmentOffset: 13;
-  unsigned moreFragments: 1;
-  unsigned dontFragme: 1;
-  unsigned ff0: 1;//ignored
+  union {
+    struct {
+      unsigned fragmentOffset: 13;
+      unsigned moreFragments: 1;
+      unsigned dontFragme: 1;
+      unsigned ff0: 1;//ignored
+    };
+    uint16_t packed;
+  } fragger;
   uint16_t uselessID;
 
   uint16_t headerChecksum;
@@ -145,20 +191,17 @@ struct IPV4Header {
   IPV4Address destIP;
 
 protected:
-  uint8_t *myWord(unsigned int wordOffset) { return (reinterpret_cast<uint8_t *>(this) + 4 * wordOffset); }
+  uint8_t *myWord(unsigned int wordOffset) {
+    return (reinterpret_cast<uint8_t *>(this) + 4 * wordOffset);
+  }
 
-  unsigned chunkLength(uint16_t total, unsigned int header) const { return static_cast<unsigned int>(4 * (total - header)); }
+  unsigned chunkLength(uint16_t total, unsigned int header) const {
+    return static_cast<unsigned int>(4 * (total - header));
+  }
 
 public:
   /** @returns pointer to bytes following base part of header */
-  Block<uint8_t> options() {
-    if (headerLength > 5) {
-      return {chunkLength(headerLength, 5), myWord(5)};
-    } else {
-      return {};
-    }
-  }
-
+  Block<uint8_t> options();
 
 /** @returns length of and pointer to bytes following base part of header */
   Block<uint8_t> payload() {
@@ -182,25 +225,31 @@ struct TCPHeader {
   AckValue seq;
   AckValue ack;
   uint16_t window;
-  unsigned FIN: 1;
-  unsigned SYN: 1;
-  unsigned RST: 1;
-  unsigned PSH: 1;
-  unsigned ACK: 1;
-  unsigned URG: 1;
-  unsigned ECE: 1;
-  unsigned CWR: 1;
+  struct {
+    unsigned FIN: 1;
+    unsigned SYN: 1;
+    unsigned RST: 1;
+    unsigned PSH: 1;
+    unsigned ACK: 1;
+    unsigned URG: 1;
+    unsigned ECE: 1;
+    unsigned CWR: 1;
+  } f;
   unsigned : 4;//reserved
-  unsigned offset: 4;//*4 to get byte offset of data from end of header
+  unsigned offset: 4;//NB *4 to get byte offset of data from end of header
+
   uint16_t urgent;
   uint16_t checksum;
+} PACKED;
 
-  uint8_t *options() {
-    return reinterpret_cast<uint8_t *>(this) + sizeof(TCPHeader);
-  };
+struct TcpEthernet {
+  EthernetHeader ethernetHeader;
+  IPV4Header ipv4Header;
+  TCPHeader tcpHeader;
 
-  uint8_t *data() {
-    return options() + offset * 4;
-  }
+  /** options follow header, might be zero length */
+  DataBlock options();
 
-};
+  /** payload follows options, which might be zero length and ditto for the payload. */
+  DataBlock payload(unsigned totalEthernetLength);
+}PACKED;
