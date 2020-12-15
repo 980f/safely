894c7daedd9c514ee01dd3ddb6771ba47e68ffde
andyAtART <andy.980f@gmail.com>
2020-12-15 17:12:31 -0600

    refine automatic timestamper

--- cppext/tracer.h
+++ cppext/tracer.h
@@ -8,7 +8,7 @@
  * A traceback buffer, useful for tracking state changes when breakpoints screw up the process.
  *
  * @param TraceItem class must be copy assignable.
- * if @param Ticked is true then it must have a 'tick' member of some integral type and this class will timestamp the object.
+ * if @param Ticked is true then it must have a 'tick' member of some integral type and this class will timestamp the object using SystemTimer (cortexm SysTick).
  * */
 template<class TraceItem, unsigned depth = 100, bool Ticked = false> class Tracer {
   TraceItem memory[depth];
@@ -18,13 +18,13 @@ public:
   /** the argument type passed to this is usually a member of the TraceItem class, so usage involves wrapping in {} so that a constructor can fill out the object and timestamp it for classes that support Ticked.*/
   void operator()(TraceItem &&braced) {
     if constexpr (Ticked) {
-      braced.tick = SystemTimer::snapLongTime();
+      braced.tick =  sizeof(braced.tick>4)? SystemTimer::snapLongTime(): SystemTimer::tocks();
     }
     looper.next() = braced;
   }
 
+  /** if you want ticks you have to provide them explicitly if you use this method */
   void operator()(const TraceItem &braced) {
-//    can't alter a const to set the tick
     looper.next() = braced;
   }
 };
