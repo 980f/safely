99ec0bafd65c91a07acfaaedf3659171f8d96b13
980f <andy.980f@gmail.com>
2021-10-26 13:17:43 -0500

    new attempt at managing path like strings

---
+++ cppext/pathiterator.h
@@ -0,0 +1,48 @@
+#ifndef PATHPARSER_H
+#define PATHPARSER_H
+
+#include "buffer.h"
+#include "halfopen.h" //includes Span
+#include "converter.h"
+/** chop up a string incrementally, overwriting separators with nulls as we go along.
+This is extracted from the batch mode version DottedName/SegmentedName/PathParser.
+
+todo: make empty field elision optional
+*/
+
+class PathIterator {
+  /** path description/state */
+  struct Rules {
+    /** what to put between items as they are concatenated */
+    char slash; //maydo: unicode char, after the basics are well tested
+    /** whether to add a trailing slash */
+    bool after;
+    /** whether to start with the slash */
+    bool before;
+    /** whether immediately succesive slashes get treated as a single (//=>/)*/
+    bool coalesce;
+    /** using most frequent by runtime instances settings as defaults */
+    Rules(char slash='/', bool after = false,bool before = false,bool coalesce=true);
+    bool isSlash(const char token){
+      return token==slash;
+    }
+  };
+
+  struct FullRules :public Rules {
+    Converter &converter;
+    FullRules();
+  };
+
+public:
+  Rules bracket;
+  PathIterator(char seperator);//initializes bracket member
+  /** called when receiver is empty, to detect quantity of leading slashes, our exception to the rule of treating multiples as a single (for http and the like) */
+  unsigned start(Indexer<const char> &scan);
+
+  /** @returns indexes identifying substring, excluding seperator. the returned Span will be empty() when the packed buffer is exhausted.
+   * the buffer ordinal may be used to identify leading slash, although attempts have been made to be smarter than that. */
+  Span next(Indexer<const char> &packed);
+
+};
+
+#endif // PATHPARSER_H
