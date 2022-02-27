da5c82e9af09f8b7e9667e8bcb7f320431aedeb0
980f <andy.980f@gmail.com>
2022-02-26 20:07:50 -0600

    find window wherever it may be

--- rasbpi/xdowrapper.h
+++ rasbpi/xdowrapper.h
@@ -5,43 +5,76 @@
 extern "C" {
 #include "xdo.h"
 }
+#include "stdlib.h" //free
 
 class XdoWrapper {
-
-    xdo_t* wrapped;
+  xdo_t* wrapped;
+  int errcode=0;
 public:
   XdoWrapper();
-  ~XdoWrapper(){
-      if(wrapped){
-          xdo_free(wrapped);
-          wrapped=nullptr;
-      }
+  ~XdoWrapper();
+
+  /** @returns whether a display has been connected. If not many functions will fail silently.
+   * Note: failure to connect to a display leaves our internal pointer null */
+  operator bool (){
+    return wrapped!=nullptr;
   }
 
-  class Keystroker { //funny spelling due to lack of namespace in xdo.h
+  /** for when constructor fails and you want to try again later */
+  bool attach(const char *displayname);
+
+  struct WindowSearch {
+    XdoWrapper &xdo;
+    WindowSearch(  XdoWrapper &xdo):xdo(xdo){}
+    Window *data=nullptr;
+    unsigned int count=0;
+    ~WindowSearch(){
+      free(data);//is calloc'd by xdo_search
+      //in case of use after free;
+      data=nullptr;
+      count=0;
+    }
+    bool operator()(const xdo_search_t &criteria){
+      xdo.errcode= xdo_search_windows(xdo.wrapped, &criteria, &data, &count);
+      return //neweer lib: xdo.errcode==0&&
+          count>0;
+    }
+    Window operator [](unsigned which){
+      if(which<count){
+        return data[which];
+      }
+      return 0;
+    }
+  };
+
+  /** keystrokes etc. need a window as well as an xdo accessor */
+  class Windoe {
       XdoWrapper &xdo;
       Window windowID;
   public:
-      Keystroker(XdoWrapper &parent):xdo(parent){
-          windowID=CURRENTWINDOW;
+      Windoe(XdoWrapper &parent);
+
+      operator bool() const {
+        return windowID!=0;  // a valule of zero is accepted by some commands as 'current_window' but if we have attached properlywe will have an actual window id number
       }
+
+      void attachTo(Window windowID){
+        this->windowID=windowID;
+      }
+
+      bool attachClass(const char *classname);
 
       /** sends one keystroke. simple ascii chars are their own keysym, but this thing needs a string not a char*/
-      void sendXkey(const char *xkeysym){
-          xdo_send_keysequence_window(xdo.wrapped,windowID,xkeysym,0);
-      }
-
-      void operator()(char sea){
-          char stringer[2];
-          stringer[0]=sea;
-          stringer[1]=0;
-          sendXkey(stringer);
-      }
+      bool sendXkey(const char *xkeysym);
+      /** sends one ascii character */
+      bool operator()(char sea);
 
   };
 
-  Keystroker keystroker(){
-      return Keystroker(*this);
+
+
+  Windoe keystroker(){
+    return Windoe(*this);
   }
 
 };
