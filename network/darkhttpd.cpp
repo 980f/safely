d8b362373ebea7a64381c7096143864fb64cc1c4
andyh <andy.980f@gmail.com>
2024-12-14 10:54:45 -0600

    import darkhttpd source, to convert into a C++ module

---
+++ network/darkhttpd.cpp
@@ -0,0 +1,6 @@
+/**
+// Created by andyh on 12/14/24.
+// Copyright (c) 2024 Andy Heilveil, (github/980f). All rights reserved.
+*/
+
+#include "darkhttpd.h"
---
+++ network/darkhttpd.h
@@ -0,0 +1,11 @@
+/**
+// Created by andyh on 12/14/24.
+// Copyright (c) 2024 Andy Heilveil, (github/980f). All rights reserved.
+*/
+
+#pragma once
+
+
+class darkhttpd {
+
+};
