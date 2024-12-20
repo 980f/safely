1d629bfcc3cdbe3dff5e22e5e3c154c9c27876dd
andyh <andy.980f@gmail.com>
2024-12-19 18:08:07 -0600

    heh, fstat has already been wrapped, added link lookup to it

--- posix/fileinfo.cpp
+++ posix/fileinfo.cpp
@@ -1,27 +1,27 @@
 //"(C) Andrew L. Heilveil, 2017"
 #include "fileinfo.h"
 #include "logger.h"
-#include "string.h"
 
-FileInfo::FileInfo(TextKey filename):PosixWrapper (filename){
-  if(nonTrivial(filename)){
+
+FileInfo::FileInfo(TextKey filename): PosixWrapper(filename) {
+  if (nonTrivial(filename)) {
     from(filename);
   }
 }
 
-FileInfo::FileInfo(int fd,const char *whatfor):PosixWrapper (whatfor){
+FileInfo::FileInfo(int fd, const char *whatfor): PosixWrapper(whatfor) {
   from(fd);
 }
 
-bool FileInfo::from(TextKey filename){
-  if(failed(stat(filename,&finfo))){
-    dbg("stat(%s) failed, errno:%d (%s)",filename,errornumber,errorText());
+bool FileInfo::from(TextKey filename, bool noFollow) {
+  if (failed(noFollow ? lstat(filename, &finfo) : stat(filename, &finfo))) {
+    dbg("stat(%s) failed, errno:%d (%s)", filename, errornumber, errorText());
     return false;
   } else {
     return true;
   }
 }
 
-bool FileInfo::from(int fd){
-  return ok(fstat(fd,&finfo));
+bool FileInfo::from(int fd) {
+  return ok(fstat(fd, &finfo));
 }
--- posix/fileinfo.h
+++ posix/fileinfo.h
@@ -1,31 +1,64 @@
-#ifndef FILEINFO_H
-#define FILEINFO_H "(C) Andrew L. Heilveil, 2017"
+#pragma once
+// "(C) Andrew L. Heilveil, 2017"
+
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <linux/limits.h>
 
 #include "sys/stat.h"
 #include "posixwrapper.h"
 #include "textkey.h"
+#include"textpointer.h"
 
-class FileInfo: public PosixWrapper {
+class FileInfo : public PosixWrapper {
 public:
   struct stat finfo;
+
   /** calls @see from on @param filename */
   FileInfo(TextKey filename);
-  /** calls @see from on @param filename */
-  FileInfo(int fd, const char *whatfor);
+
+  /** calls @see from on @param fd . @param whatfor is passed to debug log */
+  FileInfo(int fd, const char *whatfor = "FileInfo");
+
   /** @returns whether @param filename 's info was successfully acquired*/
-  bool from(TextKey filename);
+  bool from(TextKey filename, bool noFollow = false);
+
   /** @returns whether @param fd 's info was successfully acquired, file does NOT have to be open.*/
   bool from(int fd);
 
-  operator bool()const {
+  operator bool() const {
     return isOk();
   }
 
-  __off_t size()const {
+  __off_t size() const {
     return finfo.st_size;
   }
-  //add more inline functions to rename the sometimes vague stat member names.
 
+  /** @returns whether this file has a link somewhere in its name.*/
+  bool isLinked() const {
+    return finfo.st_nlink > 0;
+  }
+
+  /** @return the 'real' name of what the given name linked to, with 'this' having the other attributes of the link itself */
+  Text getLink(TextKey filename) {
+    if (from(filename, true)) {
+      /* Add one to the link size, so that we can determine whether the buffer returned by readlink() was truncated.
+         Some magic symlinks under (for example) /proc and /sys report 'st_size' as zero. In that case, take PATH_MAX as a "hope this works" estimate. */
+      auto bufsiz = (finfo.st_size > 0 ? finfo.st_size : PATH_MAX) + 1;
+      char *target = static_cast<char *>(malloc(bufsiz));
+      if (target) {
+        auto nbytes = readlink(filename, target, bufsiz);
+        if (nbytes >= 0) {
+          target[nbytes] = 0;
+        } else {
+          target[bufsiz] = 0; //to hae a chance at debugging what happened.
+        }
+        return Text(target, true);
+      } else {
+        failure(ENOMEM);
+        return Text();
+      }
+    }
+  }
 };
-
-#endif // FILEINFO_H
