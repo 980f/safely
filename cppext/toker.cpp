6794cb963dca4627de737ff908fbf20668381da0
andyh <andy.980f@gmail.com>
2024-12-22 18:37:46 -0600

    relearning str... functions

---
+++ cppext/toker.cpp
@@ -0,0 +1,7 @@
+#pragma once
+/**
+// Created by andyh on 12/22/24.
+// Copyright (c) 2024 Andy Heilveil, (github/980f). All rights reserved.
+*/
+
+#include "toker.h"
---
+++ cppext/toker.h
@@ -0,0 +1,12 @@
+#pragma once
+/**
+// Created by andyh on 12/22/24.
+// Copyright (c) 2024 Andy Heilveil, (github/980f). All rights reserved.
+*/
+
+#pragma once
+
+
+class Toker {
+
+};
