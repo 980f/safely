17721df91e54495a5a591665558d69e09c3f8185
andyh <andy.980f@gmail.com>
2024-12-22 18:38:28 -0600

    see previous commit

--- cppext/toker.cpp
+++ cppext/toker.cpp
@@ -5,3 +5,42 @@
 */
 
 #include "toker.h"
+
+#include <cstring>
+
+bool Tokenizer::hasNext(const char *delim, const char *whitespace) {
+  if (!cursor || *cursor == 0) {
+    return false;
+  }
+  if (whitespace) {
+    auto skips = strspn(cursor, whitespace);
+    cursor += skips;
+    if (*cursor == 0) {
+      return false; //ignore trailing 'whitespace', it is not an empty token.
+    }
+  }
+  end = strpbrk(cursor, delim);
+  if (end) {
+    terminator = *end; //might be a null char, this guy doesn't care.
+    //now erase trailing whitespace but not interior
+    for (char *eraser = end; strchr(whitespace, *eraser);) {
+      *eraser-- = 0;
+    }
+    //do NOT erase whitespace after the delimiter, the definition of subsequent whitespace might change contingent on the delim or token.
+    if (terminator == 0) {
+      end = nullptr; //for debug
+    } else {
+      *end = 0;
+      if (terminator == '\r' && end[1] == '\n') {
+        *++end = 0;
+      }
+    }
+  }
+  return true;
+}
+
+char *Tokenizer::fetch() {
+  auto answer = cursor;
+  cursor = end && *end ? ++end : nullptr;
+  return answer;
+}
--- cppext/toker.h
+++ cppext/toker.h
@@ -6,7 +6,24 @@
 
 #pragma once
 
+/** how tokenizing should be done, no statics under the hood like using strtok. **/
+class Tokenizer {
+  char *cursor;
+  char *end;
+  char terminator = 0;
 
-class Toker {
+public:
+  Tokenizer(char *starting): cursor{starting}, end{nullptr} {}
 
+  /** @returns whether another token found, call @see fetch to get it.
+   * next char from delim found after moving starting past anything in @param whitespace (which can be whatever you like, just typically whitespace and CRLF  */
+  bool hasNext(const char *delim, const char *whitespace = nullptr);
+
+  /** after hasNext returns true, this @returns whether the token was framed by the given @param which */
+  bool endedWith(char which) const {
+    return terminator == which;
+  }
+
+  /** must be called once each time that hasNext returns true in order to move the parser along */
+  char *fetch();
 };
