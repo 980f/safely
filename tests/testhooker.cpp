a3d9c77c6f2cf57eb0b37e1b3dc08b914c4756b1
andyh <andy.980f@gmail.com>
2025-01-08 10:44:39 -0600

    apparently I needed to readd this and hook.h to the now 2 back commit

--- tests/testhooker.cpp
+++ tests/testhooker.cpp
@@ -1,17 +1,37 @@
-#include "hook.h"
+#include "/r/work/safely/cppext/hook.h"
+
+#include <iostream>
 
 void voidfunct(int ignore){
+  std::cout<<"voidfunct->"<<ignore<<std::endl;
 }
 
 int intfunct(int ignore){
-  return 0;
+  return ignore;
+}
+
+int anotherfunct(int ignore){
+  return ignore*17;
 }
 
 int main(int argc, char *argv[]){
   Hook<int> hook(voidfunct);
 
-  Hooker<int,int> hook1(15, intfunct);
+  Hooker<int,int> hook1(100, intfunct);
 
-  Hooker<void ,int> hook2(0, voidfunct);//fails due to trying to declare a void defaultReturn.
+ // Hooker<void ,int> hook2(0, voidfunct);//fails due to trying to declare a void defaultReturn.
+  hook(10);
+  hook(15);
+  std::cout<<"hook1(20): "<<hook1(20)<<std::endl;
+  std::cout<<"hook1(25): "<<hook1(25)<<std::endl;
+
+  hook1 = &anotherfunct;
+
+  std::cout<<"hook1(20): "<<hook1(20)<<std::endl;
+  std::cout<<"hook1(25): "<<hook1(25)<<std::endl;
+
+  hook1=nullptr;
+  std::cout<<"hook1(20): "<<hook1(20)<<std::endl;
+  std::cout<<"hook1(25): "<<hook1(25)<<std::endl;
 
 }
