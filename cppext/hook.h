cab840308056259b4d208e6763dc9a8d0ba6919b
andyh <andy.980f@gmail.com>
2025-01-08 10:43:09 -0600

    somehow missed in prior commit, read its message

--- cppext/hook.h
+++ cppext/hook.h
@@ -3,84 +3,75 @@
 
 #include <functional>
 
+/** joint parts of @see Hook and @see Hooker, see those for what this is all about.*/
+template<typename RetType, typename... Args> class Hookbase {
+protected: //this class exists to be extended.
+  using Pointer = std::function<RetType(Args...)>;
 
+  Hookbase(Pointer fn = nullptr): pointer(fn) {}
 
-/** until I figure out how to code a void type for a return in Hooker here is a simpler version of that. Might be able to solve that with a constexper if in invopker. */
-template<typename... Args> class Hook {
-public: //expose function's type for use in arguments to be passed to this guy
-  using Pointer = std::function<void(Args...)>;
-
-private:
   Pointer pointer;
 
-public:
-  Hook(Pointer fn = nullptr): pointer(fn) {}
-
-  Pointer operator =(Pointer fn) {
+  /** set the function pointer.
+   * Note that the default value remains unchanged. This makes sense as the default is what the owner of the hook chooses, not the individual hoping to use the hook.
+   * @returns the old pointer, for those usages which are 'borrowing' the hook, or nice enough to share with previous one (for which you should consider using sigc library). */
+  Pointer assign(Pointer fn) {
     Pointer was = pointer;
     pointer = fn;
     return was;
-  }
-
-  void operator ()(Args... args) const {
-    if (pointer) {
-      pointer(args...);
-    }
   }
 
   /** @returns whether there is any point in calling this hook. (can distinguish between return of default and return that happens to match default)*/
   operator bool() const {
-    return bool(pointer);
+    return pointer != nullptr;
   }
 };
 
-/** hook with a return value.
-*/
-template<typename RetType, typename... Args> class Hooker {
-public: //expose function's type for use in arguments to be passed to this guy
-  using Pointer = std::function<RetType(Args...)>;
-
-private:
-  Pointer pointer;
-  RetType defaultReturn;
-
+/** just does something, or not. */
+template<typename... Args> class Hook : public Hookbase<void, Args...> {
 public:
-  /** can set function to call, must set what to return if no function is set. */
-  Hooker(RetType nullAction, Pointer fn = nullptr): pointer(fn), defaultReturn(nullAction) {}
-  /** set the function pointer.
-   * Note that the default value remains unchanged. This makes sense as the default is what the owner of the hook chooses, not the individual hoping to use the hook.
-   * @returns the old pointer, for those usages which are 'borrowing' the hook, or nice enough to share with previous one (for which you should consider using sigc library). */
-  Pointer operator =(Pointer fn) {
-    Pointer was = pointer;
-    pointer = fn;
-    return was;
-  }
+  using Base = Hookbase<void, Args...>;
+  using Pointer = typename Base::Pointer;
+  Hook(Pointer fn = nullptr): Base(fn) {}
 
-  /** invoke the hook with some args. */
+  void operator ()(Args... args) const {
+    if (*this) {
+      Base::pointer(args...);
+    }
+  }
+};
+
+/** hook with a return value. If nobody has taken the hook then a construction time set value is returned when the hook is invoked.
+*/
+template<typename RetType, typename... Args> class Hooker : Hookbase<RetType, Args...> {
+public: //expose function's type for use in arguments to be passed to this guy
+  using Base = Hookbase<RetType, Args...>;
+  using Pointer = typename Base::Pointer;
+
+  /** may set a function to call, must set what to return if no function is set. */
+  Hooker(RetType nullAction, Pointer fn = nullptr): Base(fn), defaultReturn(nullAction) {}
+
+  /** @returns what function that was set by construction or operator = returns , if none then a default value set only by constructor */
   RetType operator ()(Args... args) const {
-    if constexpr (std::is_same<RetType, void>::value) {
-      if (pointer) {
-        pointer(args...);
-      }
+    if (*this) {
+      return Base::pointer(args...);
     } else {
-      if (pointer) {
-        return pointer(args...);
-      } else {
-        return defaultReturn;
-      }
+      return defaultReturn;
     }
   }
 
-  /** @returns whether there is any point in calling this hook. (so that you can distinguish between return of default and return that happens to match default)*/
-  operator bool() const {
-    return bool(pointer);
+  Pointer operator=(Pointer fn) {
+    return Base::assign(fn);
   }
+
+private:
+  RetType defaultReturn;
 };
 
 
 /** reserve namespace for a Hooker that defaults to passing its arg through rather than feeding a constant */
-template<typename InAndOout> using Filter = Hooker<InAndOout, InAndOout>;
+template<typename InAndOut> using Filter = Hooker<InAndOut, InAndOut>;
 
-template<typename InAndOout> InAndOout trivialFilter(InAndOout in) {
+template<typename InAndOut> InAndOut trivialFilter(InAndOut in) {
   return in;
 }
