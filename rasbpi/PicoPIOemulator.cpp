e602a54fc90d680ca6388d23f4bff797aacc326e
andyh <andy.980f@gmail.com>
2025-01-11 13:06:37 -0600

    about halfway there for initial crafting, plenty more will be needed to make a simulator such as having threads for each sm

--- rasbpi/PicoPIOemulator.cpp
+++ rasbpi/PicoPIOemulator.cpp
@@ -4,3 +4,268 @@
 */
 
 #include "PicoPIOemulator.h"
+#include <bitbanger.h>
+#include <bit> //rotate instructions are still new to C++, despite being present in nearly every microcomputer ever, at least since hte early 1970's when C was invented.
+
+void PicoPIOemulator::SM::Fifo::push(unsigned value) {
+  if (!isFull()) {
+    mem[++writepointer] = value;
+    ++quantity;
+  }
+  //else do we do anything? overrun?
+}
+
+unsigned PicoPIOemulator::SM::Fifo::pull() {
+  if (!isEmpty()) {
+    --quantity;
+    return mem[readpointer++];
+  }
+  return mem[readpointer];//guessing at what hardware does, caller should check isEmpty before pull'ing.
+}
+
+void PicoPIOemulator::SM::Fifo::setSize(unsigned size) {
+  if (changed(memSize, size)) {
+    //forget the content.
+    quantity = 0;
+    readpointer = writepointer = 0;
+    //but we won't bother clearing the memory itself.
+  }
+}
+
+unsigned PicoPIOemulator::SM::GPIO::input(unsigned lsb, unsigned numBits) const {
+  //actually had to be a right rotate,the lsbs normally discarded go to the msbs.
+  return std::rotr(pin, lsb) & fieldMask(numBits);
+}
+
+void PicoPIOemulator::SM::GPIO::output(unsigned lsb, unsigned numBits, unsigned value) {
+  //todo: theoretically we might write to the lsbs of gpio with a numbits+lsb exceeds 32, but there are 2 not implemented bits, 30,31, making that useless.
+  insertField(pin, value, lsb + numBits - 1, lsb);
+}
+
+void PicoPIOemulator::SM::ShiftRegister::load(uint32_t value) {
+  SR = value;
+  consumed = 0;
+}
+
+void PicoPIOemulator::SM::ShiftRegister::input(unsigned numBits, bool fromMsb, unsigned source) {
+  if (fromMsb) {
+    SR >>= numBits;
+    SR |= (source & fieldMask(numBits, 0)) << (size - numBits);
+  } else {
+    SR <<= numBits;
+    SR |= source & fieldMask(numBits, 0);
+  }
+  consumed += numBits; //which might go over 32, but that is apparently undefined behavior.
+}
+
+unsigned PicoPIOemulator::SM::ShiftRegister::output(unsigned numBits, bool fromMsb) {
+  //todo: what do we do if more bits than are present is asked for?
+  unsigned target = 0;
+  if (fromMsb) {
+    target = SR & fieldMask(numBits, 0);
+    SR >>= numBits;
+  } else {
+    target = SR & fieldMask(size, size - numBits);
+    SR <<= numBits;
+  }
+  consumed += numBits; //which might go over 32, but that is apparently undefined behavior.
+  return target;
+}
+
+bool PicoPIOemulator::SM::testCondition(Condition condition) {
+  switch (condition) {
+    case Always:
+      return true;
+    case Xzero:
+      return reg.X == 0;
+    case Xnzd:
+      return reg.X-- > 0;
+    case Yzero:
+      return reg.Y == 0;
+    case Ynzd:
+      return reg.Y-- > 0;
+    case XneY:
+      return reg.X != reg.Y;
+    case Pin:
+      return gpio.input(cfg.jmpPin, 1);
+    case OSRne:
+      return !reg.outputSR.hasDone(32); //not our job to worry about fractional last field?
+    default:
+      return false;
+  }
+}
+
+void PicoPIOemulator::SM::JMP(Condition condition, unsigned target) {
+  if (testCondition(condition)) {
+    reg.PC = target;
+  }
+}
+
+void PicoPIOemulator::SM::IN(unsigned source, unsigned bitCount) {
+  unsigned incoming = 0;
+  switch (source) {
+    case 0:
+      incoming = gpio.input(cfg.inputLsPin, bitCount);
+      break;
+    case 1:
+      incoming = reg.X;
+      break;
+    case 2:
+      incoming = reg.Y;
+      break;
+    case 3:
+      incoming = 0;
+      break;
+    case 4:
+      IllegalOP();
+      return;
+    case 5:
+      IllegalOP();
+      return;
+    case 6:
+      incoming = reg.inputSR.all();
+      break;
+    case 7:
+      incoming = reg.outputSR.all();
+      break;
+    default:
+      InternalError();
+      return;
+  }
+  if (cfg.inputShiftsRight) {
+    reg.inputSR.input(bitCount, cfg.inputShiftsRight, incoming);
+  }
+}
+
+bool PicoPIOemulator::SM::PULL(bool ifEmpty, bool block) {
+  if (ifEmpty) {
+    if (reg.outputSR.hasDone(cfg.pullCount)) {}
+  }
+
+
+  if (block) {
+    if (TX.isEmpty()) {
+      return true; //do nothing but signal that a WAIT is needed.
+    }
+
+    reg.outputSR.load(reg.X);
+  } else {
+    reg.outputSR.load(TX.pull());
+  }
+
+  if (TX.isEmpty()) {
+    if (ifEmpty) {
+      //do nothing
+    }
+    reg.outputSR.load(reg.X);
+  } else {
+    reg.outputSR.load(TX.pull());
+  }
+  return false;
+}
+
+void PicoPIOemulator::SM::MOV(unsigned int target, bool reverse, bool invert, unsigned source) {
+  unsigned incoming = 0;
+  switch (source) {
+    case 0:
+      incoming = gpio.input(cfg.inputLsPin, cfg.pushCount); //todo: verify count source
+      break;
+    case 1:
+      incoming = reg.X;
+      break;
+    case 2:
+      incoming = reg.Y;
+      break;
+    case 3:
+      incoming = 0;
+      break;
+    case 4:
+      IllegalOP();
+      return;
+    case 5:
+      incoming = (cfg.fifoTestRx ? RX.available() : TX.available()) > cfg.fifoThreshold;
+      break;
+    default:
+      InternalError();
+      return;
+  }
+  if (reverse) {
+    //todo: bit reverse incoming
+  }
+  if (invert) {
+    incoming = ~incoming;
+  }
+  switch (target) {
+    case 0:
+    case 1:
+      reg.X = incoming;
+      break;
+  }
+}
+
+void PicoPIOemulator::SM::run() {
+  simulating = true;
+  while (simulating) {
+    //first apply config
+    RX.setSize(cfg.allFifosRX ? 8 : cfg.allFifosTX ? 0 : 4); //setSize does a change detect, we don't need to do that here
+    TX.setSize(cfg.allFifosTX ? 8 : cfg.allFifosRX ? 0 : 4);
+
+    if (flagged(inject.haveInjection)) {//we are supposed to clear haveInjection when the instruction is over, but doing it here works fine as we don't inspect it again until the instruction is over.
+      //more on timing: clearing haveInjection indicates that the host can write a new instruction without perturbing the one in progress, and that is true at this point as the IR value they write is not the same memory location as what we use for opcode inspection.
+      reg.IR = inject.IR;
+      //and PC is untouched
+    } else {
+      reg.IR = group.mem[reg.PC]; //todo: move exec and external write to exec.
+      //do PC adjustments before we potentially execute a JMP
+      if (reg.PC == cfg.endOfLoop) {
+        reg.PC = cfg.starOfLoop;
+      } else {
+        ++reg.PC;
+      }
+    }
+    auto opoptions = extractField(reg.IR, 7, 5);
+    auto indexCount = extractField(reg.IR, 4, 0);
+    switch (extractField(reg.IR, 15, 13)) {
+      case 0:
+        JMP(static_cast<Condition>(opoptions), indexCount);
+        break;
+      case 1: //wait
+        WAIT();
+        break;
+      case 2: //in
+        IN(opoptions, indexCount);
+        break;
+      case 3: //out
+        OUT();
+        break;
+      case 4:
+        if (opoptions & 3 == 0) {
+          IllegalOP(); //V1: RX as mailboxes
+        } else {
+          if (bit(reg.IR, 7)) {
+            if (PULL(bit(reg.IR, 6), bit(reg.IR, 5))) {
+
+            }
+          } else {
+            PUSH();
+          }
+        }
+        break;
+      case 5:
+        MOV(opoptions, bit(reg.IR, 4), bit(reg.IR, 3), indexCount & 7);
+        break;
+      case 6:
+        break;
+      case 7:
+        SET();
+        break;
+      default:
+        InternalError();
+        break;
+    }
+    //side set, definitely after OUT executes
+    //then delay
+    //then any waiting
+    //now advance the PC
+  }
+}
--- rasbpi/PicoPIOemulator.h
+++ rasbpi/PicoPIOemulator.h
@@ -5,249 +5,219 @@
 */
 
 #pragma once
-#include <bitbanger.h>
-#include <cycler.h>
-#include <dout.h>
-#include <halfopen.h>
-
-#include <bit>
+#include <dout.h>  //GPIO pins?
 
 class PicoPIOemulator {
 public:
-  /** todo: ensure maximum of 29 is allowed for low and 30 for width */
-  struct BitSpan : Span {
-    BitSpan(unsigned low, unsigned width): Span(low, low + width) {}
-
-    unsigned msb() {
-      return highest - 1;
-    };
-
-    unsigned extractFrom(unsigned value) {
-      return extractField(value, msb(), lowest);
-    }
-
-    void insertInto(unsigned &target, unsigned value) {
-      mergeInto(target, value << lowest, fieldMask(msb(), lowest));
-    }
-  };
-
-  struct GPIO {
-    unsigned pin;
-    unsigned dir;
-
-    unsigned input(unsigned lowest) {
-      //actually had to be a right rotate,the lsbs normally discarded go to the msbs. Target will mask as needed
-      return std::rotr(pin, lowest);
-    }
-
-    void output(BitSpan bits, unsigned fromlsbs) {
-      //todo: import bitfield stuff rather than reproducing inline.
-      fromlsbs <<= bits.lowest;
-    }
-  } gpio;
-
-  /** for our first cut at it we are not worrying about the packing into control register words */
-  struct Config {
-    unsigned fifoThreshold;
-    bool fifoTestRx; //else tx
-    unsigned starOfLoop;
-    unsigned endOfLoop;
-    bool outIsLatched;
-    bool inIsLatched;
-    unsigned OEpin;
-    unsigned jmpPin;
-    bool sideSetOEsnotPins;
-    bool useSideSetEnable;
-    bool stalled;
-    bool autoPush;
-    bool autoPull;
-    bool inputShiftsRight;
-    bool outpuShiftsRight;
-    unsigned pushCount;
-    unsigned pullCount;
-    bool allFifosTX;
-    bool allFifosRX;
-
-    unsigned outputLsPin;
-    unsigned setLsPin;
-    unsigned sidesetLsPin;
-    unsigned inputLsPin;
-
-    unsigned numberSetPins;
-    unsigned numberSideSetPins; //max 5, if sidSetORsnotPins then maxis4
-    unsigned nyi; //for things we don't yet know.
-  } cfg;
-
-  struct ShiftRegister {
-    unsigned SR;
-    unsigned lostbits;
-    Cycler cycle;
-    /** modify SR with incoming value */
-    bool input(BitSpan bits, bool msb, unsigned value) {
-      lostbits = 0; //extract from designated end, then shift and insert given value
-
-
-      return cycle.next(); //for caller to implement auto push|pull
-    }
-
-    /** return shifted out stuff */
-    bool output(BitSpan bits, bool msb, unsigned &target);
-  };
-
-  struct Registers {
-    unsigned X;
-    unsigned Y;
-    ShiftRegister inputSR;
-    ShiftRegister outputSR;
-    unsigned PC;
-    unsigned IR;
-    unsigned clock; //diagnostic, for us to count instruction cycles and annotate output traces
-  } reg;
-
   uint16_t mem[32];
 
-  /////////////////////////////
-  enum Condition {
-    Always = 0,
-    Xzero,
-    Xnzd,
-    Yzero,
-    Ynzd,
-    XneY,
-    Pin,
-    OSRne
+  //tokens for memory mapped access by host
+  enum class SM_CBfield { //~6 words for each SM
+    CLKDIV = 0, EXECCTRL, SHIFTCTRL, ADDR, INSTR, PINCTRL,
+    BlockSize
   };
 
+  class SM {
+  public:
+    PicoPIOemulator &group;
+    unsigned ownIndex;
 
-  void InternalError() {
-    //emulator has goofed up!
-  }
+  public:
+     SM(PicoPIOemulator *group,unsigned yourIndex) : group{*group}, ownIndex {yourIndex} {}
 
-  void IllegalOP() {
-    //unimplemented opcode pattern, or otherwise officially "undefined operation"
-  }
+  private:
+    struct Fifo {
+      unsigned mem[8]; //alloate max
+      unsigned memSize = 4; //defaults to 4
+      unsigned readpointer = 0;
+      unsigned writepointer = 0;
+      unsigned quantity = 0;
 
-  bool testCondition(Condition condition) {
-    switch (condition) {
-      case Always:
-        return true;
-      case Xzero:
-        return reg.X == 0;
-      case Xnzd:
-        return reg.X-- != 0;
-      case Yzero:
-        return reg.Y == 0;
-      case Ynzd:
-        return reg.Y-- != 0;
-      case XneY:
-        return reg.X != reg.Y;
-      case Pin:
-        return gpio.input(BitSpan(cfg.jmpPin, 1));
-      case OSRne:
-        return reg.outputSR.SR != 0; //probably should be counter, not data
-      default:
-        return false;
+      bool isFull() {
+        return quantity == memSize;
+      }
+
+      bool isEmpty() {
+        return quantity == 0;
+      }
+
+      void push(unsigned value);
+
+      unsigned pull();
+
+      unsigned available() {
+        return quantity;
+      }
+
+      void setSize(unsigned size);
+    } RX, TX;
+
+    /** driver can push instructions at the device. This is checked at the start of every instruction execution.*/
+    struct InstructionInjector {
+      uint16_t IR;
+      bool haveInjection;
+
+      void operator()(uint16_t opcode) {
+        haveInjection = true;
+        IR = opcode;
+      }
+    } inject;
+
+    struct GPIO {
+      unsigned pin = 0;
+      unsigned dir = 0;
+
+      unsigned input(unsigned lsb, unsigned numBits) const;
+
+      void output(unsigned lsb, unsigned numBits, unsigned value);
+    } gpio;
+
+    /** for our first cut at it we are not worrying about the packing into control register words */
+    struct Config {
+      unsigned fifoThreshold = 0;
+      bool fifoTestRx = 0; //else tx
+      unsigned starOfLoop = 0;
+      unsigned endOfLoop = 31;
+      bool outIsLatched = 0;
+      bool inIsLatched = 0;
+      unsigned OEpin = 0;
+      unsigned jmpPin = 0;
+      bool sideSetOEsnotPins = 0;
+      bool useSideSetEnable = 0;
+      bool stalled = 0;
+      bool autoPush = 0;
+      bool autoPull = 0;
+      bool inputShiftsRight = 0;
+      bool outpuShiftsRight = 0;
+      unsigned pushCount = 0;
+      unsigned pullCount = 0;
+      bool allFifosTX = 0;
+      bool allFifosRX = 0;
+
+      unsigned outputLsPin = 0;
+      unsigned setLsPin = 0;
+      unsigned sidesetLsPin = 0;
+      unsigned inputLsPin = 0;
+
+      unsigned numberSetPins = 0;
+      unsigned numberSideSetPins = 0; //max 5, if sidSetORsnotPins then maxis4
+      unsigned nyi = ~0; //for things we don't yet know.
+    } cfg;
+
+    class ShiftRegister {
+      uint32_t SR = 0;
+      const int size = std::numeric_limits<decltype(SR)>::digits;
+      unsigned consumed = 0;
+
+    public:
+      void load(uint32_t value);
+
+      /** @returns whether at least @param numBits has been shifted since the last load.
+       * The user can confuse themselves by having the threshold not be a multiple of the bit quantities being moved by input/output.
+       */
+      bool hasDone(unsigned numBits) {
+        return consumed >= numBits;
+      }
+
+      /** modify SR with incoming bit field */
+      void input(unsigned numBits, bool fromMsb, unsigned source);
+
+      /** return shifted out stuff */
+      unsigned output(unsigned numBits, bool fromMsb);
+
+      unsigned all() {
+        return SR;
+      }
+    };
+
+    struct Registers {
+      //the following are definitley unsigned, a signed int will not work!
+      unsigned X = 0;
+      unsigned Y = 0;
+      ShiftRegister inputSR;
+      ShiftRegister outputSR;
+      unsigned PC = 0;
+      unsigned IR = 0; //separate from what pico writes to so that we can use two actual threads for emulation and get real realtime troubles.
+      unsigned clock = 0; //diagnostic, for us to count instruction cycles and annotate output traces
+    } reg;
+
+    /////////////////////////////
+    enum Condition {
+      Always = 0,
+      Xzero,
+      Xnzd,
+      Yzero,
+      Ynzd,
+      XneY,
+      Pin,
+      OSRne
+    };
+
+
+    void InternalError() {
+      //emulator has goofed up!
     }
-  }
 
-  void JMP(Condition condition, unsigned target) {
-    if (testCondition(condition)) {
-      reg.PC = target;
+    void IllegalOP() {
+      //unimplemented opcode pattern, or otherwise officially "undefined operation"
     }
-  }
 
-  void IN(unsigned source, unsigned bitCount) {
-    unsigned incoming = 0;
-    switch (source) {
-      case 0:
-        incoming = gpio.input(BitSpan(cfg.inputLsPin, bitCount));
-        break;
-    }
-    if (cfg.inputShiftsRight) {
-      reg.inputSR.SR = incoming;
-      // if (reg.inputSR.input(cfg.inputBits, cfg.inputDirection, gpio.input(cfg.inputBits)));
-    }
-  }
+    /** @returns wheter to jump, including doing the decrements on scratch registers. */
+    bool testCondition(Condition condition);
+
+    void JMP(Condition condition, unsigned target);
+
+    void IN(unsigned source, unsigned bitCount);
 
 
-  void PUSH() {}
+    void PUSH() {}
 
-  void PULL() {}
+    /** @returns whether we need to wait on !TX.isEmpty */
+    bool PULL(bool ifEmpty, bool block);
 
-  void WAIT();
+    void WAIT();
 
-  //from register to pins
-  void OUT();
+    //from register to pins
+    void OUT();
 
-  //from instruction to pins
-  void SET() {}
+    //from instruction to pins
+    void SET() {}
 
-  void MOV(unsigned int target, bool reverse, bool invert, unsigned source) {
-    unsigned incoming = 0;
-    switch (source) {
-      case 0:
-        incoming = gpio.input(cfg.inputLsPin);
-        break;
-      default:
-        InternalError();
-        return;
-    }
-    if (reverse) {
-      //todo: bit reverse incoming
-    }
-    if (invert) {
-      incoming = ~incoming;
-    }
-    switch (target) {
-      case 0:
-      case 1:
-        reg.X = incoming;
-        break;
-    }
-  }
+    void MOV(unsigned int target, bool reverse, bool invert, unsigned source);
 
-  void run() {
-    reg.IR = mem[reg.PC]; //todo: move exec and external write to exec.
-    auto opoptions = extractField(reg.IR, 7, 5);
+    bool simulating;
 
-    auto indexCount = extractField(reg.IR, 4, 0);
-    switch (extractField(reg.IR, 15, 13)) {
-      case 0:
-        JMP(static_cast<Condition>(opoptions), indexCount);
-        break;
-      case 1: //wait
-        WAIT();
-        break;
-      case 2: //in
-        IN(opoptions, indexCount);
-        break;
-      case 3: //out
-        OUT();
-        break;
-      case 4:
-        if (opoptions & 3 == 0) {
-          IllegalOP(); //V1: RX as mailboxes
-        } else {
-          if (bit(reg.IR, 7)) {
-            PULL();
-          } else {
-            PUSH();
-          }
-        }
-        break;
-      case 5:
-        MOV(opoptions, bit(reg.IR, 4), bit(reg.IR, 3), indexCount & 7);
-        break;
-      case 6:
-        break;
-      case 7:
-        SET();
-        break;
-      default:
-        InternalError();
+    void run();
+  }; //end SM
+
+  SM sm[4]={SM(this,0),SM(this,1),SM(this,2),SM(this,3)};
+
+
+  //back to PIO itself
+
+
+  enum RegisterName {
+    CTRL = 0,
+    FSTAT, //full and empty bits for each SM's fifos
+    FDEBUG, //stalls and overflows, host can clear stall although pushing or pulling data would seem more appropriate
+    FLEVEL, //fifo levels for each fifo
+    TXfifo, //push/pull access to each SM's tx fifo
+    RXfifo = TXfifo + 4,
+    IRQ = RXfifo + 4, IRQ_FORCE,
+    //gpio intereface:
+    INPUT_SYNC_BYPASS, DBG_PADOUT, DBG_PADOE, DBG_CFGINFO,
+    INSTR_MEM,
+    SM_cb,
+    INTR = SM_cb + 4 * unsigned(SM_CBfield::BlockSize),
+    IRQ0_EFS,
+    IRQ1_EFS = IRQ0_EFS + 3,
+  };
+
+  //read only for now
+  unsigned operator[](unsigned index) {
+    switch (index) {
+      case CTRL:
         break;
     }
-    //side set, definitely after OUT executes
-    //then delay
   }
 };
