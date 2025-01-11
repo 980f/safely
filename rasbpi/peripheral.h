ce08d8e70c6017c4ce711b3a968a86e22883ca83
andyh <andy.980f@gmail.com>
2025-01-10 19:35:28 -0600

    c++20 strictness on volatile made usage of it painful. user of peripheral reg[] will have to atomicize/thread synch any actual multiple updates to such values

--- rasbpi/peripheral.h
+++ rasbpi/peripheral.h
@@ -5,7 +5,7 @@
 class Peripheral{
 public://until we wrap with block accessors
   /*each peripheral has a block of address space. We cannot directly address it, we use a memory mapper object by which the OS gives us an address that will modify the physical one. */
-  Mapped<volatile unsigned> reg;
+  Mapped<unsigned> reg;//volatile here was very syntactically expensive, and most of the time either unnecessary or we really wanted atomic update rather than averting caching a value. Only in a polling loop would the 'volatile' matter.
 public:
   /** blockOffset is a byte address, blockLength is number of 32 bit registers, not number of bytes/ */
   Peripheral(unsigned blockOffset, unsigned blockLength);
