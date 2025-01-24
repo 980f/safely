71ff18d666b9e724f9946a2f6b3fc22a3496b899
andyh <andy.980f@gmail.com>
2025-01-23 21:23:26 -0600

    even more extractions of source files.

--- posix/fcntlflags.h
+++ posix/fcntlflags.h
@@ -1,7 +1,7 @@
 #ifndef FCNTLFLAGS_H
 #define FCNTLFLAGS_H
 
-/** linux specific trick to get the O_READ and simillar flags without also getting all the function definitions.
+/** linux specific trick to get the O_READ and similar flags without also getting all the function definitions.
  * This is useful when your module has to pass such flags on to a module which actually uses them, but otherwise should not access the fcntl features directly.
  * Compilers are now fast enough that that shouldn't matter, but this helps purify the suggestions lists of my IDE.
  */
