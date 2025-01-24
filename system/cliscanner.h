e22287d6a47118a8409d9be400821891da761678
andyh <andy.980f@gmail.com>
2025-01-23 20:52:33 -0600

    more extractions of source files.

--- system/cliscanner.h
+++ system/cliscanner.h
@@ -1,4 +1,3 @@
-
 /**
 // Created by andyh on 1/22/25.
 // Copyright (c) 2025 Andy Heilveil, (github/980f). All rights reserved.
@@ -16,12 +15,13 @@ public:
   CliScanner(int argc, char **argv) : argc{argc}, argv{argv} {}
 
   bool stillHas(unsigned needed) const {
-    return argi + needed < argc;
+    return argi + needed < argc; //test: seems off by one.
   }
 
   char *operator()() {
     return argv[argi++];
   }
+
   template<typename StringAssignable> bool operator>>(StringAssignable &target) {
     if (argi < argc) {
       if constexpr (std::is_same<StringAssignable, bool>::value) {
@@ -30,24 +30,28 @@ public:
       } else if constexpr (std::is_integral<StringAssignable>::value) {
         char *end;
         target = strtoll(argv[argi++], &end, 10);
-        const bool range_error = errno == ERANGE;
-        if (range_error) {
+        if (errno == ERANGE) {
           return false; //and leave errno as is
         }
-        //todo: SI letters
+        //todo: SI letters id end not trivial.
       } else { //type must have an operator=(char *);
         target = argv[argi++];
       }
       return true;
+    } else if constexpr (std::is_floating_point<StringAssignable>::value) {
+      char *end;
+      target = strtod(argv[argi++], &end);
+      //todo: test errno
+      //todo: si letters?
+      return true;
     } else {
-      errno = EDOM;
+      errno = EDOM; //todo: this might not be the traditional error code for "index out of range"
       return false;
     }
   }
 
-
 private:
   const int argc;
-  char ** const argv;
+  char **const argv;
   int argi = 0;
 };
