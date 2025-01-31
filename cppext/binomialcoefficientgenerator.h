f8dbdb5f3d9273bf63b875343f0090285cbe4344
andyh <andy.980f@gmail.com>
2025-01-31 17:17:39 -0600

    much better name

--- cppext/binomialcoefficientgenerator.h
+++ cppext/binomialcoefficientgenerator.h
@@ -1,15 +1,12 @@
+#pragma once
 /**
 // Created by andyh on 1/31/25.
 // Copyright (c) 2025 Andy Heilveil, (github/980f). All rights reserved.
-*/
-
-#pragma once
-
-/* initialized with n, produces Cnr for r= 0 .. n inclusive, sequentially
+ * One of these when initialized with n, produces Cnr for r= 0 .. n inclusive, sequentially
  *
  * tested 31jan2005 via godbolt.org
  */
-class CombinatorialGenerator {
+class BinomialCoefficientGenerator {
   unsigned n;
   unsigned r;
   unsigned Cnr;
@@ -30,12 +27,11 @@ public:
     Cnr = 1;
   }
 
-  CombinatorialGenerator(unsigned enn) {
+  BinomialCoefficientGenerator(unsigned enn) {
     setN(enn);
   }
 
   unsigned operator()() {
-    //if r==0 increment and return
     unsigned precalced = Cnr;
     Cnr *= n-r;//n,n-1,n-2 : n-r
     Cnr /= ++r;//1,1,2,3,4 : r but not if 0 unless n/0 = n on your processor!
