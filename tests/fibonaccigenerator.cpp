b1204f52f0ad743c349644cdf89adc1cf8a4a710
andyh <andy.980f@gmail.com>
2025-01-31 23:37:40 -0600

    proper termination condition.

--- tests/fibonaccigenerator.cpp
+++ tests/fibonaccigenerator.cpp
@@ -1,4 +1,3 @@
-
 /**
 // Created by andyh on 1/31/25.
 // Copyright (c) 2025 Andy Heilveil, (github/980f). All rights reserved.
@@ -7,17 +6,18 @@
 #include "fibonaccigenerator.h"
 #include "cliscanner.h"
 #include <iostream>
+
 void testFibongenerator(int argc, char *argv[]) {
   CliScanner argparser(argc, argv);
-  unsigned even=0;
-  unsigned odd=1;
+  unsigned even = 0;
+  unsigned odd = 1;
   if (argparser.stillHas(2)) {
-    argparser>>even;
-    argparser>>odd;
+    argparser >> even;
+    argparser >> odd;
   }
-  FibonacciGenerator<unsigned,true> fibber(even,odd);
-  std::cout<<"Generating fibonacci numbers starting with \t"<<even<<"\t"<<odd<<std::endl;
+  FibonacciGenerator fibber(even, odd);
+  std::cout << "Generating fibonacci numbers starting with \t" << even << "\t" << odd << std::endl;
   do {
-    std::cout<<"\t"<<fibber();
-  } while(fibber.ordinal()<200);//todo: add cli arg for limit,
+    std::cout << "\t" << fibber();
+  } while (fibber && fibber.ordinal() < 200); //todo: add cli arg for limit,
 }
