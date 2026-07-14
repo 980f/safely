cf9d40d57be14d57ce0af6ec792dbcd9411d2b39
andyh <andy.980f@gmail.com>
2026-07-14 11:23:19 -0500

    fully repaired

--- cppext/continuedfractionratiogenerator.cpp
+++ cppext/continuedfractionratiogenerator.cpp
@@ -2,42 +2,29 @@
 #include "continuedfractionratiogenerator.h"
 #include "minimath.h"
 
+/*
+ element 0 holds provisional new value
+ element 1 holds last computed value
+ element 2 holds value before that of element 1
+ In the wikipedia article these match the subscripts -1, and -2, just drop the article's minus signs.
+
+ */
+
 ContinuedFractionRatioGenerator::ContinuedFractionRatioGenerator() {
-  //confused the pee out of debug, and the thing is not usable anyway restart(0);
+  //confused the pee out of debug, and the thing is not usable anyway: restart(0);
   fraction = 0.0;
 }
 
 static void shift(unsigned hk[3]) {
-  hk[1] = hk[0];
-  hk[2] = hk[1];
-}
-
-bool ContinuedFractionRatioGenerator::restart(double ratio, unsigned limit) {
-  this->limit = limit == 0 ? ~0U : limit; //there were too many defective call points, fixing it here.
-  if(!std::isnormal(ratio)){
-    fraction = 0; //should preclude triggering obscure faults
-    return false;
-  }
-  fraction = 1.0 / fabs(ratio);
-  split();
-
-  h[0] = k[1] = 0; //note that indices here are swap of following ones. h/k 0/1 is a 2x2 matrix that we are initializing to the identity matrix.
-  h[1] = k[0] = 1;
-  //the following should not actually get used, but prevent getting distracted during debug:
-  h[2] = h[1];
-  k[2] = k[1];
-
-  shift(h);
-  shift(k);
-  k[0]=1;
-  h[0]=an;
-  return step(); //gives int(ratio)/1
+  hk[2] = hk[1];//prior last value becomes 2 back
+  hk[1] = hk[0];//most recent computed value becomes 'prior'
+  //hk[0] is now stale, we leave it untouched for debug.
 }
 
 bool ContinuedFractionRatioGenerator::bump(unsigned hk[3]) {
-  uintmax_t provisional = uintmax_t(an) * hk[1] + hk[2]; //uintmax_t: using extra bits to not have to deal with overflow here.
-  if (provisional <= limit) {
-    hk[0] = unsigned(provisional);
+  auto provisional = an * hk[1] + hk[2]; //uintmax_t: using extra bits to not have to deal with overflow here.
+  if (provisional <= limit) { //using <= versus just < is why the limit value is usually "all 1's" in the ValueType.
+    hk[0] = provisional; //a truncation, but one we just checked to insure that it doesn't actually truncate.
     return true;
   }
   return false;
@@ -56,19 +43,6 @@ bool ContinuedFractionRatioGenerator::step() {
   return false;
 }
 
-double ContinuedFractionRatioGenerator::approximation() {
-  return ratio(double(numerator()), double(denominator()));
-}
-
-void ContinuedFractionRatioGenerator::run() {
-  //we use a counter to guard against bad input parameters. Termination is normally done via step() due to the 'limit' value which is default set to note overflow in the computation.
-  for (unsigned steps = maxWorkingBits; steps-- > 0 && step();) {}
-}
-
-double ContinuedFractionRatioGenerator::best() {
-  run();
-  return approximation();
-}
 
 bool ContinuedFractionRatioGenerator::split() {
   if (fraction == 0.0) {
@@ -85,3 +59,39 @@ bool ContinuedFractionRatioGenerator::split() {
   fraction = inverse;
   return true;
 }
+
+bool ContinuedFractionRatioGenerator::restart(double ratio, unsigned limit) {
+  this->limit = limit == 0 ? ~0U : limit; //there were too many defective call points, fixing it here.
+  h[0]=k[0]=0;//4 debug
+  if(!std::isnormal(ratio)){
+    an = 0;//4 debug
+    fraction = 0; //should preclude triggering obscure faults
+    return false;
+  }
+  fraction = fabs(ratio);
+  split();
+
+  h[2]=0; h[1]=1;
+  k[2]=1; k[1]=an;
+
+  return true;
+}
+
+double ContinuedFractionRatioGenerator::approximation() const {
+  return ratio(double(numerator()), double(denominator()));
+}
+
+double ContinuedFractionRatioGenerator::better() const {
+  return (approximation()+ratio(double(h[2]), double(k[2])))/2;
+}
+
+void ContinuedFractionRatioGenerator::run() {
+  //we use a counter to guard against bad input parameters. Termination is normally done via step() due to the 'limit' value which is default set to note overflow in the computation.
+  for (unsigned steps = maxWorkingBits; steps-- > 0 && step();) {}
+}
+
+double ContinuedFractionRatioGenerator::best() {
+  run();
+  return approximation();
+}
+
--- cppext/continuedfractionratiogenerator.h
+++ cppext/continuedfractionratiogenerator.h
@@ -1,14 +1,17 @@
 #pragma once
 
 
+#include <cstdint>
 #ifndef __cpp_lib_bit_cast // C++ >= 20
-  /// copied and compacted from recent <bit>
-  constexpr double  bit_cast( unsigned long __from) noexcept { return __builtin_bit_cast(double, __from); }
+/// copied and specialized and compacted from recent <bit>
+constexpr double bit_cast(unsigned long __from) noexcept {
+  return __builtin_bit_cast(double, __from); }
 #else
 #include <bit>
 #endif // __cpp_lib_bit_cast
 
-#include <limits>
+#include <limits>  //bits in the working type
+
 /** find a ratio of integers which best matches a floating point number.
  * This is useful for tuning a PWM.
  *
@@ -16,26 +19,28 @@
  * One can extend it with something that computes the estimate on each pass and records the best as the iteration seeks it. That will nearly double the computational burden so we won't do that in this class.
  * Another variation would be a class for trying to reduce a ratio of integers to fit into a smaller integer representation.
  *
- * Note: during development the ratio was easily inverted by minor changes to the initialization, which leans a tad illegitimately on the iterative step to save on some code.
- * The terse variable names match the wikipedia article this math was extracted from.
+ * Note: during development the ratio was easily inverted by minor changes to the initialization, which depended a tad illegitimately on the iterative step to save on some code.
+ * The terse variable names match the wikipedia article this math was extracted from. Search 'simple continued fraction'.
  */
 class ContinuedFractionRatioGenerator {
 public:
   using ValueType=unsigned; //could use a template argument, but then the whole of the class would have to be in the header. Instead use a limit appropriate for a shorter unsigned type to get the best ratio for that type.
   constexpr static const unsigned maxWorkingBits = std::numeric_limits<ValueType>::digits;
-  constexpr static const double epsilon = bit_cast((1023ul-maxWorkingBits)<<52);
+  constexpr static const double epsilon = bit_cast((1023ul-maxWorkingBits)<<52);//IEEE_64 specific pow(2,-x)
 
 public:
   double fraction;
-  unsigned an;
-  //built by recursion relationship, where each step depends on prior 2, so we need a memory that is 3 deep.
-  ValueType h[3];
-  ValueType k[3];
+  uintmax_t an;//the next coefficient of the continued fraction, using maximum precision
   /** how large the numerator or denominator may be.
    * The minimal constraint is to not overflow the computer representation.
-   * This is usually a power of 2.
+   * This is usually a power of 2 - 1.
    */
   ValueType limit;
+
+  //built by recursion relationship, where each step depends on prior 2, so we need a memory that is 3 deep.
+  ValueType h[3]; //cumulative numerator
+  ValueType k[3]; //cumulative denominator
+
 
   /** create one for learning the algorithm, then call restart followed by looping on step inspecting numerator and denominator as you go. */
   ContinuedFractionRatioGenerator();
@@ -54,17 +59,20 @@ public:
   bool step();
 
   /** @returns a number greater than denominator if input value is greater than 1 */
-  ValueType numerator() {
-    return k[1];
-  }
-
-  /** @returns a number greater than numerator if input value is less than 1 */
-  ValueType denominator() {
+  ValueType numerator() const {
     return h[1];
   }
 
-  /** best approximation, used for testing the algorithm, the desired output is  */
-  double approximation();
+  /** @returns a number greater than numerator if input value is less than 1 */
+  ValueType denominator() const {
+    return k[1];
+  }
+
+  /** current approximation, used for testing the algorithm */
+  double approximation() const;
+
+  /** the average of the last 2 is always closer to the original than either of them */
+  double better() const;
 
   void run();
 
